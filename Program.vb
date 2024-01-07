b60c0e7d081a27e8c02fe07ec22528b2e362ba12
Diego Vargas <diego.vargas.j@ing.uchile.cl>
2024-01-07 11:47:13 -0300

    test2

--- Program.vb
+++ Program.vb
@@ -1,8 +1,5 @@
-Imports System
 
 Module Program
     Sub Main(args As String())
-        Dim webClient As New System.Net.WebClient
-        Dim result As String = webClient.DownloadString("http://10.10.14.174:8000")
     End Sub
 End Module
